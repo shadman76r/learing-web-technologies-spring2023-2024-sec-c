f270b485e529ef9e4c214164d57af2999976de57
Shadman_Shakib <100066452+shadman76r@users.noreply.github.com>
2024-03-07 16:31:54 +0600

    Add files via upload

---
+++ lab-7/product.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Product</title>
+</head>
+<body>
+    <form action="">
+        <fieldset>
+            <legend><strong>ADD PRODUCT</strong></legend>
+                Name <br> 
+                <input type="text" name="" id="">
+                <br>
+                Buying Price <br>
+                <input type="text" name="buy_price" id="">
+                <br>
+                Selling Price  <br>
+                <input type="text" name="Selling_price" id="">
+                <br>
+                <hr>
+                <input type="checkbox" name="display" id=""> Display <br>
+                <hr>
+                <input type="submit" name="submit" placeholder="SAVE" id="">
+                <br>
+        </fieldset>
+    </form>
+    
+</body>
+</html>
