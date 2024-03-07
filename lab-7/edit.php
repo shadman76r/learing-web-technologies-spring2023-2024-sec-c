9da06ac755b8cee8eb9d39c758b7bc1a33af2981
Shadman_Shakib <100066452+shadman76r@users.noreply.github.com>
2024-03-07 16:33:20 +0600

    Add files via upload

---
+++ lab-7/edit.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit product</title>
+</head>
+<body>
+
+   <form method="post" action="">
+   <fieldset>
+            <legend><strong>EDIT PRODUCT</strong></legend>
+                Name <br> 
+                <input type="text" name="" id="">
+                <br>
+                Buying Price <br>
+                <input type="text" name="buy_price" id="">
+                <br>
+                Selling Price  <br>
+                <input type="text" name="Selling_price" id="">
+                <br>
+                <hr>
+                <input type="checkbox" name="display" id=""> Display <br>
+                <hr>
+                <input type="submit" name="submit"  id="" value="save">
+                <br>
+        </fieldset>
+   </form>
+</body>
+</html>
